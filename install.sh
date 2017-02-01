ce82cb8a9d450edb27bebf60b1a388d2af55214f
Lenny Truong <leonard@truong.io>
2017-02-01 10:14:05 -0800

    Update

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p $HOME/.config/nvim
+ln -s `pwd`/init.vim $HOME/.config/nvim/init.vim
