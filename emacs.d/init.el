3bb241559509f6bc37403355e4aceeeea33cb3c1
Lenny Truong <lenny@stanford.edu>
2018-09-29 10:00:43 -0700

    Install base packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,79 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 (require 'evil)
 (evil-mode 1)
+
+(require 'evil-terminal-cursor-changer)
+(unless (display-graphic-p)
+  (setq evil-visual-state-cursor 'box) ; █
+  (setq evil-insert-state-cursor 'bar) ; ⎸
+  (setq evil-emacs-state-cursor 'hbar) ; _
+  )
+
+(evil-commentary-mode)
+
+
+(require 'helm-config)
+(require 'helm-projectile)
+(setq projectile-mode-line "Projectile")
+(projectile-global-mode)
+(helm-projectile-on)
+
+(global-evil-leader-mode)
+(evil-leader/set-key
+ "x" 'helm-M-x
+ "m" 'helm-mini
+ "b" 'helm-buffers-list
+ "y" 'helm-show-kill-ring
+ "i" 'helm-semantic-or-imenu
+ "o" 'helm-occur
+ "a" 'helm-projectil-ag
+ "y" 'helm-show-kill-ring
+ "c" 'compile
+ "r" 'recompile
+ "e s" 'eshell
+ "g s" 'magit-status
+ "g p s" 'magit-push
+ "g p l" 'magit-pull
+ "g c" 'magic-commit
+ "f" 'helm-find-files)
+
+(define-key evil-normal-state-map (kbd "C-p") 'helm-projectile)
+
+(require 'magit)
+(evil-add-hjkl-bindings magit-log-mode-map 'emacs)
+(evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+(evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+  "K" 'magit-discard-item
+  "L" 'magit-key-mode-popup-logging)
+(evil-add-hjkl-bindings magit-status-mode-map 'emacs
+  "K" 'magit-discard-item
+  "l" 'magit-key-mode-popup-logging
+  "h" 'magit-toggle-diff-refine-hunk)
+
+(setq-default sml/theme 'dark)
+(sml/setup)
 
 (load-theme 'nord t)
