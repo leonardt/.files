b8b52403114a198e773a24866ce0442b7383cd19
Lenny Truong <lenny@stanford.edu>
2018-11-19 18:46:09 -0800

    Activate evil cursor terminal changer

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -30,9 +30,13 @@
 
 (require 'evil-terminal-cursor-changer)
 (unless (display-graphic-p)
+  (setq evil-motion-state-cursor 'box)  ; █
+  (setq evil-visual-state-cursor 'box)  ; █
+  (setq evil-normal-state-cursor 'box)  ; █
   (setq evil-visual-state-cursor 'box) ; █
   (setq evil-insert-state-cursor 'bar) ; ⎸
   (setq evil-emacs-state-cursor 'hbar) ; _
+  (evil-terminal-cursor-changer-activate)
   )
 
 (evil-commentary-mode)
