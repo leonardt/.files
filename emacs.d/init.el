ac2d101a388d07593b494d5624cc2b48d52c9e85
Lenny Truong <lenny@stanford.edu>
2019-05-21 08:26:02 -0700

    Updates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit nord-theme exec-path-from-shell evil))))
+ '(package-selected-packages
+   (quote
+    (yaml-mode magit nord-theme exec-path-from-shell evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,6 +20,8 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
+(show-paren-mode 1)
+(global-linum-mode)
 
 (require 'package)
 (add-to-list 'package-archives
@@ -44,6 +48,69 @@ re-downloaded in order to locate PACKAGE."
 (require-package 'magit)
 (require-package 'nord-theme)
 (require-package 'exec-path-from-shell)
+(require-package 'fzf)
+(require-package 'projectile)
+(require-package 'smex)
+(require-package 'clang-format)
+(require-package 'yaml-mode)
+(require-package 'dashboard)
+(require-package 'rainbow-delimiters)
+
+(dashboard-setup-startup-hook)
+
+(require 'undo-tree)
+(global-undo-tree-mode)
+(setq undo-tree-auto-save-history t)
+
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+(setq undo-tree-history-directory-alist
+      `((".*" . ,temporary-file-directory)))
+
+
+(require 'saveplace)
+(setq-default save-place t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+;; compile
+(setq-default compilation-always-kill t)
+(setq-default compilation-ask-about-save nil)
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; clean up old buffers periodically
+(require 'midnight)
+(midnight-delay-set 'midnight-delay 0)
+(setq delete-by-moving-to-trash t)
+
+(blink-cursor-mode -1)
+(electric-indent-mode t)
+(electric-pair-mode t)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(defun clang-format-buffer-smart ()
+  "Reformat buffer if .clang-format exists in the projectile root."
+  (when (f-exists? (expand-file-name ".clang-format" (projectile-project-root)))
+    (clang-format-buffer)))
+
+(defun clang-format-buffer-smart-on-save ()
+  "Add auto-save hook for clang-format-buffer-smart."
+  (add-hook 'before-save-hook 'clang-format-buffer-smart nil t))
+
+(add-hook 'c-mode-hook 'clang-format-buffer-smart-on-save)
+(add-hook 'c++-mode-hook 'clang-format-buffer-smart-on-save)
 
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
@@ -53,6 +120,12 @@ re-downloaded in order to locate PACKAGE."
       evil-want-C-w-in-emacs-state t)
 
 (require 'magit)
+(require 'fzf)
+(require 'projectile)
+(require 'smex)
+(smex-initialize)
+
+(require 'yaml-mode)
 
 (require 'evil)
 (evil-mode t)
@@ -65,6 +138,8 @@ re-downloaded in order to locate PACKAGE."
 (evil-leader/set-leader "<SPC>")
 
 (evil-leader/set-key
+ "x" 'smex
+ "f" 'fzf
  "c" 'compile
  "r" 'recompile
  "e s" 'eshell
